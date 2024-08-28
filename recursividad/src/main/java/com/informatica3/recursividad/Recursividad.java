0253ae0843feb62a253512de6222e0080e1d8a09
Gabriel Maugeri <maugerigabriel@gmail.com>
2024-08-28 20:32:55 -0300

    first commit

---
+++ recursividad/src/main/java/com/informatica3/recursividad/Recursividad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.informatica3.recursividad;
+
+/**
+ *
+ * @author mauge
+ */
+public class Recursividad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
